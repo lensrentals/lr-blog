767f6722d05833e109cf54aa99f31a0b286ab51b
Ryan Filler <ryanfiller89@gmail.com>
2019-08-26 10:14:38 -0500

    builds an example blog post

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,19 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `LR Blog Poc`,
+    description: `Is Gatsby + Netlify a good idea? Lets find out.`,
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/content`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    // ^ my plugins, v their plugins
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
