3831ca0facb0c824daa973556f3c86b00109ca56
Ryan Filler <ryanfiller89@gmail.com>
2019-08-27 11:26:15 -0500

    adds mdx example componenent

---
+++ src/components/markdown.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { MDXRenderer } from "gatsby-plugin-mdx"
+import { MDXProvider } from '@mdx-js/react'
+
+const H2 = props => {
+
+	return (
+		<h2
+			className="h2" 
+			style={{
+				padding: '.5em 1em',
+				background: 'rebeccapurple',
+				color: 'white',
+			}}
+		>
+			{props.children}
+		</h2>
+	)
+}
+
+const components = {
+	h2: H2,
+}
+
+const MarkdownBlock = (props) => {
+
+	return (
+		<MDXProvider components={components}>
+			<MDXRenderer>
+        {props.post}
+      </MDXRenderer>
+		</MDXProvider>
+	)
+}
+
+export default MarkdownBlock;
